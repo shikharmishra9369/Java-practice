c418bac707bd47227ede4541ca61271bfe32b697
Shikhar Mishra <mishrashikhar5555@gmail.com>
2026-06-16 13:43:50 +0530

    Add number pyramid pattern printing in Java
    Implement a number pyramid pattern in Java.

---
+++ number pyramid pattern.java
@@ -0,0 +1,23 @@
+//print the pattern number pyramid
+
+import java.util.*;
+
+public class patterns {
+    
+    public static void main(String args[]) {
+        int n = 5;
+        
+        for(int i=1; i<=n; i++) {
+            //spaces
+            for(int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+            //number ->print row no, row no times
+            for(int j=1; j<=i; j++) {
+                System.out.print(i + " ");
+                
+            }
+            System.out.println();
+        }
+    }
+}
