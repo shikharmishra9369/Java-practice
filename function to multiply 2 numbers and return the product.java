5d9684b4dd6cbb28980bd463f7554cf473c9ee13
Shikhar Mishra <mishrashikhar5555@gmail.com>
2026-06-20 15:32:29 +0530

    Add function to calculate product of two numbers

---
+++ function to multiply 2 numbers and return the product.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class functions {
+
+public static int calculateProduct(int a, int b){
+  return a * b;
+
+}
+public static void main(String args[]) {
+  Scanner sc = new Scanner(System.in);
+  int a = sc.nextInt();
+  int b = sc.nextInt();
+
+  System.out.println("product of 2 numbers is:"+ calculateProduct(a, b));
+
+}
+}
