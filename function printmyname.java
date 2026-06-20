776397909b565e6db24a14aaa94d5855fe8bd5de
Shikhar Mishra <mishrashikhar5555@gmail.com>
2026-06-20 14:57:17 +0530

    Add printMyName function in Java

---
+++ function printmyname.java
@@ -0,0 +1,15 @@
+//function in java
+import java.util.*;
+
+public class function {
+    public static void printMyName(String name) {
+        System.out.println(name);
+        return;
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        
+        printMyName(name); //call kiya function ko
+    }
+}
