c148c2bfc53c4e0266100ff0259ca0f46f53a482
Shikhar Mishra <mishrashikhar5555@gmail.com>
2026-07-13 16:19:01 +0530

    Implement program to find largest element in array

---
+++ Largest element of array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class array {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int arr[] = new int[5];
+    System.out.println("Enter 5 numbers:");
+    for(int i=0; i<5; i++) {
+      arr[i] = sc.nextInt();
+    }
+    int max = arr[0];
+    for(int i=1; i<5; i++) {
+      if(arr[i]>max){
+        max = arr[i];
+      }
+    }
+    System.out.println(" Larget Element= " +max);
+  }
+}
