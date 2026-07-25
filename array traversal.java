26a7a227a4cc0d8e8caa5e8fd13838285b1ee8ff
Shikhar Mishra <mishrashikhar5555@gmail.com>
2026-07-25 20:29:06 +0530

    Implement array traversal in main class

---
+++ array traversal.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class main {
+    public static void main(String[]args) {
+        int[]arr = {5,10,15,20,25};
+        for(int i=0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
